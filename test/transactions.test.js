31b393615521e77d072823fe2213194126a564a7
Mark Penovich <mpenovich1@gmail.com>
2018-10-15 15:09:13 -0500

    auth

--- test/transactions.test.js
+++ test/transactions.test.js
@@ -5,7 +5,9 @@ var shapeshift = require('../')
 test('should get a list of transactions by an API key', function (t) {
   t.plan(3)
   var somePrivKey = crypto.randomBytes(32).toString('hex')
+  console.log(somePrivKey)
   shapeshift.transactions(somePrivKey, function (err, data) {
+    console.log(data, 'THIS IS CONSOLE LOG')
     t.ifError(err, 'no error')
     t.true(Array.isArray(data), 'data is an array')
     t.same(data.length, 0, 'no data returned')
